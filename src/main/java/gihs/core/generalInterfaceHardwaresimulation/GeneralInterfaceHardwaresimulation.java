9e58b8451f8266ddf46ea69426caca897a47cced
Mohammad Dahi <mohammad95dahi@gmail.com>
2023-08-22 20:36:52 +0200

    Modified import in class GeneralInterfaceHardwaresimulation

--- src/main/java/gihs/core/generalInterfaceHardwaresimulation/GeneralInterfaceHardwaresimulation.java
+++ src/main/java/gihs/core/generalInterfaceHardwaresimulation/GeneralInterfaceHardwaresimulation.java
@@ -7,7 +7,12 @@ import gihs.core.managementOFJsonNodeALL.JsonUtil;
 import gihs.gem5.parser.Gem5Parser;
 import gihs.sniper.parser.SniperParser;
 import gihs.zsim.parser.ZsimParser;
-import org.apache.commons.cli.*;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
